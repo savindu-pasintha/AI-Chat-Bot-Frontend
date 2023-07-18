ebceb8973cea994677c7b9625c87ea195dc71fd0
savindu-pasintha <savindupasingtha@gmail.com>
2023-07-18 11:43:17 +0530

    change the layout.js

--- src/layouts/auth/layout.js
+++ src/layouts/auth/layout.js
@@ -93,7 +93,7 @@ export const Layout = (props) => {
               sx={{ mb: 3 }}
               variant="subtitle1"
             >
-              A professional kit that comes with ready-to-use MUI components.
+              we are providing you to a AI based chatbot
             </Typography>
             <img
               alt=""
